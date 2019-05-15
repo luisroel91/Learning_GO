aeb3ad5a34d33125838ccff11f93456bb38591fc
phaseon <luisroel@me.com>
2019-05-14 21:57:30 -0400

    V1 Declaring values

--- MyAgeIs.go
+++ MyAgeIs.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	var age int // declare int variable
 	fmt.Println("my age is", age)
+	age = 28 // Assign value
+	fmt.Println("my age is", age)
+	age = 54
+	fmt.Println("my new age is", age)
 }
