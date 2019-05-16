a92f3ef66fb8763aaa8bd8d583830014c0abb4f0
phaseon <luisroel@me.com>
2019-05-16 18:39:53 -0400

    Worked on Loop files

---
+++ Loops/Loops.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Here we'll define our loop. The syntax for a standard loop is:
+	//
+	// for initial var; exit condition; operation per iter {
+	//
+	// }
+	//
+	// In the below loop, counter starts at 1 and every iteration adds 1
+	// to the counter. The loops runs until the counter is greater than
+	// or equal to 500. We use an if statement to break the loop when the
+	// counter reaches 201. If its an even number, it notifies the user
+	// and continues the loop using the continue keyword.
+	//
+
+	for counter := 1; counter <= 500; counter++ {
+		fmt.Println(
+			"Counter is:", counter,
+		)
+
+		if counter >= 201 {
+			break // loops terminates when counter reaches 200
+		}
+
+		if counter%2 == 0 {
+			fmt.Println(
+				"Counter is an even number:", counter,
+			)
+			continue //continue
+		}
+
+	}
+
+}
---
+++ Loops/NestedLoops.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Nested loops, as in other programming languages
+	// are loops within a loop. The first loop (outer)
+	// prints a new line at the end of every sequence.
+	// The second loop (inner) prints an asterisk for
+	// every iteration.
+
+	total_lines := 10 // Total lines in sequence
+
+	for counter := 0; counter < total_lines; counter++ { // Print new line every iteration
+
+		for x := 0; x <= counter; x++ { // Print asterisks every iteration
+
+			fmt.Print("*")
+
+		}
+
+		fmt.Println()
+
+	}
+
+}
